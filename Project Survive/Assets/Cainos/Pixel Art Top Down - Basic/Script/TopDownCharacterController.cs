3b64b11424068cad92540059a290ad71d90cd368
paowick <korn2k9@gmail.com>
2022-11-15 19:54:23 +0700

    new system input and player direction

--- Project Survive/Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
+++ Project Survive/Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 namespace Cainos.PixelArtTopDown_Basic
 {
     public class TopDownCharacterController : MonoBehaviour
     {
+
+
         public float speed;
 
         private Animator animator;
@@ -14,30 +18,48 @@ namespace Cainos.PixelArtTopDown_Basic
 
         private string dirview = "W";
 
+        private Vector2 pointerInput;
+
+
+        [SerializeField]
+        private InputActionReference pointterPosition;
+
+
+        
+
         private void Start()
         {
-         weaponMethod = GameObject.Find("Sword").GetComponent<WeaponMethod>();
+            weaponMethod = GameObject.Find("Sword").GetComponent<WeaponMethod>();
             animator = GetComponent<Animator>();
+
         }
         private void Update(){
+
+            pointerInput = GetPointerInput();
+
+            Debug.Log(pointerInput);
+
+            if(pointerInput.x > 0){
+                animator.SetInteger("Direction", 2);
+            }else if(pointerInput.x < 0){
+                animator.SetInteger("Direction", 3);
+            }
+
+
             Vector2 dir = Vector2.zero;
             if (Input.GetKey(KeyCode.A)){
                 dir.x = -1;
-                animator.SetInteger("Direction", 3);
                 dirview = "A";
             }else if (Input.GetKey(KeyCode.D)){
                 dir.x = 1;
-                animator.SetInteger("Direction", 2);
                 dirview = "D";
             }
 
             if (Input.GetKey(KeyCode.W)){
                 dir.y = 1;
-                animator.SetInteger("Direction", 1);
                 dirview = "W";
             }else if (Input.GetKey(KeyCode.S)){
                 dir.y = -1;
-                animator.SetInteger("Direction", 0);
                 dirview = "S";
             }
 
@@ -57,6 +79,11 @@ namespace Cainos.PixelArtTopDown_Basic
             animator.SetBool("IsMoving", dir.magnitude > 0);
 
             GetComponent<Rigidbody2D>().velocity = speed * dir;
+        }
+        private Vector2 GetPointerInput(){
+            Vector3 mousePos = pointterPosition.action.ReadValue<Vector2>();
+            mousePos.z = UnityEngine.Camera.main.nearClipPlane;
+            return UnityEngine.Camera.main.ScreenToWorldPoint(mousePos);
         }
     }
 }
