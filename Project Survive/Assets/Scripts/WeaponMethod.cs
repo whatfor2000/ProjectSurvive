878b741f87a59366c27cf0ceba7e891446c01778
paowick <korn2k9@gmail.com>
2022-11-19 12:55:13 +0700

    /

--- Project Survive/Assets/Scripts/WeaponMethod.cs
+++ Project Survive/Assets/Scripts/WeaponMethod.cs
@@ -17,7 +17,7 @@ public class WeaponMethod : MonoBehaviour
     }
 
     private void Update() {
-        if(Input.GetMouseButtonDown(0)){
+        if(Input.GetMouseButton(0)){
              Attack();
             }
 
