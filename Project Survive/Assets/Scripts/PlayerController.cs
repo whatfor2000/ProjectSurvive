03c74f94abd3a36c2f525e6d68520ee979968b5f
paowick <korn2k9@gmail.com>
2022-11-23 18:33:05 +0700

    fixed

--- Project Survive/Assets/Scripts/PlayerController.cs
+++ Project Survive/Assets/Scripts/PlayerController.cs
@@ -1,47 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-        public float speed;
+         public float speed = 3;
+        private Animator animator;
+        private WeaponMethod weaponMethod;
+        private Vector2 pointerInput;
+        private WeaponParent weaponParent;
+        private bool isdodge = false;
+        private bool iframe = false;
+        [SerializeField]
+        private InputActionReference pointterPosition;
+        private void Start(){
 
-        //private Animator animator;
+            weaponParent = GetComponentInChildren<WeaponParent>();
+            weaponMethod = GameObject.Find("Sword").GetComponent<WeaponMethod>();
+            animator = GetComponent<Animator>();
 
-        private void Start()
-        {
-            //animator = GetComponent<Animator>();
+        }
+        void mousedir(){
+            if(pointerInput.x > 0){
+                GetComponent<SpriteRenderer>().flipX = false;
+            }else if(pointerInput.x < 0){
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+        private void Update(){
+
+            pointerInput = GetPointerInput();
+            weaponParent.Pointerposition = pointerInput;
+            Vector2 dir = Vector2.zero;
+            if (Input.GetAxisRaw("Horizontal") < 0){
+                dir.x = -1;
+                animator.SetBool("IsWalk",true);
+                GetComponent<SpriteRenderer>().flipX = true;
+            }else if (Input.GetAxisRaw("Horizontal") > 0){
+                dir.x = 1;
+                animator.SetBool("IsWalk",true);
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            if (Input.GetAxisRaw("Vertical") > 0){
+                dir.y = 1;
+                animator.SetBool("IsWalk",true); 
+           
+            }else if (Input.GetAxisRaw("Vertical") < 0){
+                dir.y = -1;
+                animator.SetBool("IsWalk",true); 
+                
+            }
+            if(Input.GetAxisRaw("Vertical").Equals(0)  && Input.GetAxisRaw("Horizontal").Equals(0)){
+                animator.SetBool("IsWalk",false);
+            }
+
+            if(Input.GetAxisRaw("Jump").Equals(1) && isdodge == false){
+                speed = 10;
+                animator.SetBool("isdodge",true);
+                iframe = true;
+                isdodge = true;
+                StartCoroutine(Delay());
+            }
+
+            if(Input.GetMouseButton(0)){
+                GetComponentInChildren<WeaponMethod>().Attack();
+            }
+            if(GetComponentInChildren<WeaponMethod>().attackBlocked){
+              mousedir();  
+            }
+
+            
+          
+            dir.Normalize();
+            GetComponent<Rigidbody2D>().velocity = speed * dir;
         }
 
-
-        private void Update()
-        {
-            Vector2 dir = Vector2.zero;
-            if (Input.GetKey(KeyCode.A))
-            {
-                dir.x = -1;
-                //animator.SetInteger("Direction", 3);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                dir.x = 1;
-                //animator.SetInteger("Direction", 2);
-            }
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                dir.y = 1;
-                //animator.SetInteger("Direction", 1);
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                dir.y = -1;
-                //animator.SetInteger("Direction", 0);
-            }
-
-            dir.Normalize();
-            //animator.SetBool("IsMoving", dir.magnitude > 0);
-
-            GetComponent<Rigidbody2D>().velocity = speed * dir;
+        private IEnumerator Delay(){
+        yield return new WaitForSeconds(0.2f);
+        iframe = false;
+        speed = 3;
+        animator.SetBool("isdodge",false);
+        yield return new WaitForSeconds(2);
+        isdodge = false;
+        }
+        private Vector2 GetPointerInput(){
+            Vector3 mousePos = pointterPosition.action.ReadValue<Vector2>();
+            mousePos.z = UnityEngine.Camera.main.nearClipPlane;
+            return UnityEngine.Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
         }
 }
